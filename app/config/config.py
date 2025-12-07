3de71aa6f72a45a86c382d9a6662972048dfb5ae
fprestamo <francisco.prestamo.2003@gmail.com>
2025-12-07 11:15:44 -0500

    feat: update IP addresses in middleware configuration for database and backend

--- app/config/config.py
+++ app/config/config.py
@@ -4,8 +4,8 @@ from core.middleware import Middleware
 
 
 ip_cache = {
-    "db": ["196.168.0.2","196.168.0.3 ","196.168.0.4", "196.168.0.5", "196.168.0.6"],
-    "backend": ["196.168.0.7", "196.168.0.8"]
+    "db": ["172.18.0.2","172.18.0.3 ","172.18.0.4", "172.18.0.5", "172.18.0.6"],
+    "backend": ["172.18.0.7", "172.18.0.8"]
 }
 
 middleware = Middleware(timeout=30.0, csv_paths=CSV_PATHS, ip_cache=ip_cache)
