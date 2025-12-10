b40af070ad4ff2f4b6dec6ae0fdb9519ac9e874f
fprestamo <francisco.prestamo.2003@gmail.com>
2025-12-09 23:00:56 -0500

    feat: clear IP cache in middleware configuration for database and backend

--- app/config/config.py
+++ app/config/config.py
@@ -4,8 +4,8 @@ from core.middleware import Middleware
 
 
 ip_cache = {
-    "db": ["172.18.0.2","172.18.0.3 ","172.18.0.4", "172.18.0.5", "172.18.0.6"],
-    "backend": ["172.18.0.7", "172.18.0.8"]
+    "db": [],
+    "backend": []
 }
 
 middleware = Middleware(timeout=30.0, csv_paths=CSV_PATHS, ip_cache=ip_cache)
